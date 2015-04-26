b59f0eb5928852936888e2cb3673db33322c92b7
wms1993 <1482824390@qq.com>
2015-04-27 00:07:10 +0800

    第一次

---
+++ src/com/wms/bowen/MyView.java
@@ -0,0 +1,15 @@
+package com.wms.bowen;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class MyView extends LinearLayout{
+
+	public MyView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	
+
+}
